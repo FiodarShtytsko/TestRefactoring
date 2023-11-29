5ee89a456a8a8af61298f260cef527a63642be7c
Fiodar Shtytsko <shtytsko777@gmail.com>
2023-11-29 16:32:09 +0100

    style refactor in 'MainViewModel'

--- RefactoringProject/CodingSession/Main/MainViewModel.swift
+++ RefactoringProject/CodingSession/Main/MainViewModel.swift
@@ -10,7 +10,7 @@ import UIKit
 
 final class MainViewModel {
     private(set) var cellViewModels: [MainCollectionViewCellModel] = []
-
+    
     func fetchVideoAssets(completion: @escaping () -> Void) {
         let fetchOptions = PHFetchOptions()
         fetchOptions.predicate = NSPredicate(format: "mediaType == %d", PHAssetMediaType.video.rawValue)
@@ -18,7 +18,7 @@ final class MainViewModel {
         let fetchResult = PHAsset.fetchAssets(with: fetchOptions)
         var tempViewModels = [MainCollectionViewCellModel]()
         let group = DispatchGroup()
-
+        
         fetchResult.enumerateObjects { [weak self] (asset, _, _) in
             group.enter()
             self?.loadImage(for: asset) { image in
@@ -29,26 +29,29 @@ final class MainViewModel {
                 group.leave()
             }
         }
-
+        
         group.notify(queue: .main) {
             self.cellViewModels = tempViewModels
             completion()
         }
     }
+    
+}
 
-    private func loadImage(for asset: PHAsset, completion: @escaping (UIImage?) -> Void) {
+private extension MainViewModel {
+    func loadImage(for asset: PHAsset, completion: @escaping (UIImage?) -> Void) {
         let manager = PHImageManager.default()
         let requestOptions = PHImageRequestOptions()
         requestOptions.isSynchronous = false
         requestOptions.deliveryMode = .highQualityFormat
         let targetSize = CGSize(width: UIScreen.main.bounds.width / 3, height: UIScreen.main.bounds.width / 3)
-
+        
         manager.requestImage(for: asset, targetSize: targetSize, contentMode: .aspectFill, options: requestOptions) { (image, _) in
             completion(image)
         }
     }
-
-    private func formatDuration(_ duration: TimeInterval) -> String {
+    
+    func formatDuration(_ duration: TimeInterval) -> String {
         let formatter = DateComponentsFormatter()
         formatter.allowedUnits = [.hour, .minute, .second]
         formatter.zeroFormattingBehavior = [.pad]
